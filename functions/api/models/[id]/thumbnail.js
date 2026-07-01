0c80b335a467bd0e75406675ca86b87703eb30fd
camliesj <2390748035@qq.com>
2026-07-01 16:27:10 +0800

    feat: add model thumbnail endpoint

---
+++ functions/api/models/[id]/thumbnail.js
@@ -0,0 +1,24 @@
+import { ApsClient } from "../../../_lib/aps.js";
+import { getModel } from "../../../_lib/db.js";
+import { handleErrors, methodNotAllowed } from "../../../_lib/http.js";
+
+export function onRequestGet({ env, params }) {
+  return handleErrors(async () => {
+    const model = await getModel(env.DB, params.id);
+    if (!model.urn) {
+      const error = new Error("Model upload has not been completed");
+      error.status = 400;
+      throw error;
+    }
+
+    const image = await new ApsClient(env).getThumbnail(model.urn);
+    return new Response(image.body, {
+      headers: {
+        "Content-Type": image.contentType || "image/png",
+        "Cache-Control": "public, max-age=300"
+      }
+    });
+  });
+}
+
+export function onRequest() { return methodNotAllowed(); }
